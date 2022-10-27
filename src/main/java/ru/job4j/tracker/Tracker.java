77886a475c7c360589ed488710bac9459550f4f6
ivansvyatobog <kelthasaugust@yandex.ru>
2022-10-27 20:39:38 +0300

    Tracker - хранилище

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -1,5 +1,7 @@
 package ru.job4j.tracker;
 
+import java.util.Arrays;
+
 public class Tracker {
     private final Item[] items = new Item[100];
     private int ids = 1;
@@ -21,5 +23,29 @@ public class Tracker {
             }
         }
         return rsl;
+    }
+
+    public Item[] findByName(String key) {
+        Item[] result = new Item[size];
+        int count = 0;
+        for (int index = 0; index < this.size; index++) {
+            if (items[index].getName().equals(key)) {
+                result[index] = items[index];
+                count++;
+            }
+        }
+        return Arrays.copyOf(result, count);
+    }
+
+    public Item[] findAll() {
+        Item[] result = new Item[size];
+        int count = 0;
+        for (int index = 0; index < this.size; index++) {
+            if (items[index] != null) {
+                result[index] = items[index];
+                count++;
+            }
+        }
+        return Arrays.copyOf(result, count);
     }
 }
