91d9815cc0913bb7c03c820d8a4d28e574990e20
ivansvyatobog <kelthasaugust@yandex.ru>
2022-10-30 12:25:22 +0300

    Метод удаления заявки Tracker.delete

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -58,12 +58,11 @@ public class Tracker {
     public boolean delete(int id) {
         int index = indexOf(id);
         boolean validate = index != -1;
+        int length = size - index - 1;
         if (validate) {
-            int start = index + 1;
-            int length = size - index - 1;
             items[size - 1] = null;
             size--;
-            System.arraycopy(items, start, items, index, length);
+            System.arraycopy(items, index + 1, items, index, length);
         }
         return validate;
     }
