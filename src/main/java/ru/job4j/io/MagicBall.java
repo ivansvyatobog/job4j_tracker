71647d4f319d0b7f9adf62a1eede5287d1d816b1
ivansvyatobog <kelthasaugust@yandex.ru>
2022-10-31 10:51:45 +0300

    Чтение из консоли. Класс Scanner.

--- src/main/java/ru/job4j/io/MagicBall.java
+++ src/main/java/ru/job4j/io/MagicBall.java
@@ -1,4 +1,5 @@
 package ru.job4j.io;
+
 import java.util.Random;
 import java.util.Scanner;
 
@@ -10,7 +11,8 @@ public class MagicBall {
         int answer = new Random().nextInt(3);
         if (answer == 0) {
             System.out.println("Да");
-        } if (answer == 1) {
+        }
+        if (answer == 1) {
             System.out.println("Нет");
         } else {
             System.out.println("Может быть");
