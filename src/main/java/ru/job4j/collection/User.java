b5c22f452566a6ad0babcf9b03f4bbcb23d5040d
ivansvyatobog <kelthasaugust@yandex.ru>
2022-11-21 10:39:04 +0300

    Организовать сортировку User

---
+++ src/main/java/ru/job4j/collection/User.java
@@ -0,0 +1,41 @@
+package ru.job4j.collection;
+
+import ru.job4j.tracker.Item;
+
+import java.util.Comparator;
+import java.util.Objects;
+
+public class User implements Comparable<User> {
+    private String name;
+
+    private int age;
+
+    public User(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        User user = (User) o;
+        return age == user.age
+                && Objects.equals(name, user.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, age);
+    }
+
+    @Override
+    public int compareTo(User o) {
+        int result = this.name.compareTo(o.name);
+        return result == 0 ? Integer.compare(this.age, o.age) : result;
+    }
+}
